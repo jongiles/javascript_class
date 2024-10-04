919c6070f4263577586ae4b50491ec7bf4eb0493
Jonathan Giles <jgiles@Jonathans-MacBook-Pro.local>
2024-10-04 13:45:42 -0400

    finish looped and random looped table

--- homework_two/script.js
+++ homework_two/script.js
@@ -1,39 +1,57 @@
 
 //Part 1     
 
-document.write('<table border=1>');
-document.write('<tr>');
-document.write('<th bgcolor=cyan>X </th>');
 
-for (header = 1; header <= 10; header++){
-        
-    document.write('<th bgcolor=cyan>' + header + '</th>');           
+maxRow = Math.random();
+maxCol = Math.random();
+maxRow = maxRow * 10 + 1;
+maxCol = maxCol * 10 + 1;
 
-}
 
-document.write('</tr>');
+function makeMyTable(numRows, numCols){
 
-for (row = 1; row <= 10; row++){
-
+    document.write('<table border=1>');
     document.write('<tr>');
-    document.write('<th <td bgcolor=cyan>' , row , '</th>');
-
-    var col = 1;
-    var data;
-
-    while( col <= 10){
-
-        data = col * row
-        document.write('<td>' + data + '</td>');
-        col++;			
-
+    document.write('<th bgcolor=cyan>X </th>');
+    
+    for (header = 1; header <= numCols; header++){
+            
+        document.write('<th bgcolor=cyan>' + header + '</th>');           
+    
     }
-
+    
     document.write('</tr>');
-    	
+    
+    for (row = 1; row <= numRows; row++){
+    
+        document.write('<tr>');
+        document.write('<th <td bgcolor=cyan>' , row , '</th>');
+    
+        var col = 1;
+        var data;
+    
+        while( col <= numCols){
+    
+            data = col * row
+            document.write('<td>' + data + '</td>');
+            col++;			
+    
+        }
+    
+        document.write('</tr>');
+    
+    }
+    
+    document.write('</table>');
+    
+
 }
 
-document.write('</table>');
+document.write('<br></br>')
+makeMyTable(10,10)
+
+document.write('<br></br>')
+makeMyTable(maxRow,maxCol)
 
 //Part 2
 
